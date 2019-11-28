415242ad644526efe45325d846614e1787938bc2
Fernando García <olfativo@hotmail.com>
2019-11-28 15:34:45 +0100

    Add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Alert from 'react-bootstrap/Alert';
 
 const App: React.FC = () => {
   return (
@@ -8,16 +9,10 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Alert variant="success">
+            Oh hai
+          </Alert>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
